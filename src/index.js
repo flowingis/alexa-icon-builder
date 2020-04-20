8e72c25082a4dbdbfbcfa88579336c3f82a17720
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2020-04-20 13:23:30 +0200

    showing preview

--- src/index.js
+++ src/index.js
@@ -1,35 +1,47 @@
 import createImage from './createImage.js'
 
-const input = document
-    .querySelector('input')
-
 const SMALL_FILENAME = 'it-IT_smallIconUri.png'
 const SMALL_SIZE = 108
 
 const BIG_FILENAME = 'it-IT_largeIconUri.png'
 const BIG_SIZE = 512
 
+const input = document
+  .querySelector('input')
+
+const image = document
+  .querySelector('img')
+
+input.addEventListener('change', event => {
+  const fileReader = new window.FileReader()
+  fileReader.onload = () => {
+    image.src = fileReader.result
+  }
+  const [file] = input.files
+  fileReader.readAsDataURL(file)
+})
+
 document
-    .querySelector('button')
-    .addEventListener('click', () => {
-        const [file] = input.files
+  .querySelector('button')
+  .addEventListener('click', () => {
+    const [file] = input.files
 
-        if(!file){
-            window.alert('Nessun file selezionato')
-            return
-        }
+    if (!file) {
+      window.alert('Nessun file selezionato')
+      return
+    }
 
-        createImage(file, SMALL_SIZE)
-            .then(download(SMALL_FILENAME))
+    createImage(file, SMALL_SIZE)
+      .then(download(SMALL_FILENAME))
 
-        createImage(file, BIG_SIZE)
-            .then(download(BIG_FILENAME))
-    })
+    createImage(file, BIG_SIZE)
+      .then(download(BIG_FILENAME))
+  })
 
 const download = filename => url => {
-    var link = document.createElement('a')
-    link.download = filename
-    link.href = url
-    link.click()
-    return url
+  const link = document.createElement('a')
+  link.download = filename
+  link.href = url
+  link.click()
+  return url
 }
